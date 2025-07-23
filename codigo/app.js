a11eb94e137048a16fb0bfbad23562a2fa5c1b36
Pili203 <pilmaria2000@gmail.com>
2025-07-23 14:53:40 -0500

    Actualice app.js

--- codigo/app.js
+++ codigo/app.js
@@ -1,24 +1,56 @@
- function repartir(){let tablero=document.getElementById("tablero");
-            const TOTAL_CARTAS = 10;
-            for(let i=0; i < TOTAL_CARTAS; i++) {
-                let carta=document.createElement("img");
-                let indice=Math.floor(Math.random()*52) + 1;
-                carta.src="imagenes/cartas/Carta"+indice+".jpg";
-                tablero.appendChild(carta);
-                
+ let repartio= false;
+let cartas=[];
+
+function repartir() {
+    let tablero = document.getElementById("tablero");
+    tablero.innerHTML="";
+    const TOTAL_CARTAS = 10;
+    for (let i = 0; i < TOTAL_CARTAS; i++) {
+        let carta = document.createElement("img");
+        let indice = Math.floor(Math.random() * 52) + 1;
+        cartas.push(indice);
+        carta.src = "imagenes/cartas/Carta" + indice + ".jpg";
+        tablero.appendChild(carta);
+    }
+    const audio = new Audio("sonidos/repartir.mp3");
+    audio.play().then(() => {
+        window.alert("Se han repartido las 10 cartas");
+    });
+repartio=true;
+}
+
+function verificar(){
+    if(!repartio) {
+       window.alert("No se han repartido las cartas");
+    }
+    else {
+        //iniciar los contadores en 0
+        contadores=new Array(13).fill(0);
+        //recorrer la lista de las cartas
+        for (let i = 0; i < cartas.length; i++) {
+            let posicion=cartas[i] % 13;
+            if(posicion==0){
+                posicion = 12;
             }
-             const audio=new Audio("sonidos/repartir.mp3");
-             audio.play().then(() => {
-                window.alert("se han repartido las 10 cartas");
-             });
+            else {
+                posicion--;
+            }
+            contadores[posicion]++;
+         }
+         nombreCartas=["As", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "Jack", "Queen", "king"];
+         //recorrer los contadores
+         let mensaje="se encontraron los siguientes grupos:\n";
+         grupos=["vacio", "Non", "Par", "Terna", "Cuarta", "Quinta", "Sexta", "Séptima", "Octava", "Novena", ];
+         for (let i = 0; i < contadores.length; i++){
+            if (contadores[i] >=2) {
+            mensaje += grupos[contadores[i]] + " de " + "\n";
+            mensaje += "Hay " + contadores[i] + " cartas de " + nombreCartas[i]+"\n";
+         }
 
-             function verificar(){
-                window.alert("mañana chequearemos las cartas");
-             }
-        }
-
-        
-           
-        
-
-
+         }
+         window.alert(mensaje);
+    
+    
+    
+    }
+}
